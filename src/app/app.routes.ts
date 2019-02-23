0518ed5e34de31c229e8b6bdd7de1054aa3720f1
maciejniedzielski <niedzielamac@gmail.com>
2019-02-23 17:04:10 +0100

    feat(home.component.ts): Add home component & route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './containers/home/home.component';
 
 export const AppRoutes: Routes = [
-  { path: '', component: AppComponent },
-  { path: '**', redirectTo: '' }
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { 
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Login' }
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
