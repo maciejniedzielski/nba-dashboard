b76faad7ae79beb7c4d1411d9208c81f05b82f70
Maciej Niedzielski <mniedzielski@sigma.ug.edu.pl>
2019-03-04 20:22:30 +0100

    fix(navigation.component.ts): Close opened menu on navigation end event

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavigationPaths } from 'src/app/app.routes';
+import { Router, NavigationEnd } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
+
   public navigationElements = NavigationPaths;
   public isMenuToggled: boolean = false;
 
-  constructor() { }
+  private subGuard$: Subject<boolean> = new Subject<boolean>();
 
-  ngOnInit() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      takeUntil(this.subGuard$)
+    ).subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        this.isMenuToggled = false;
+      }
+    });
+  }
 
   public toggleMenu(): void {
     this.isMenuToggled = !this.isMenuToggled;
   }
 
+  ngOnDestroy(): void {
+    this.subGuard$.next(true);
+  }
 }
