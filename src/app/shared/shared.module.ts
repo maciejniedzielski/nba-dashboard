27e015b31660dfe14afaf712ce942a798fc78b59
maciejniedzielski <niedzielamac@gmail.com>
2019-03-12 18:53:34 +0100

    feat(Standings): Add conference change buttons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [
     MatSortModule,
     MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ],
   exports: [
     MatSortModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ]
 })
 export class SharedModule { }
